1a302b44ef8f3920577a195401e2107aac52ca30
unknown <josedanielgrn01@gmail.com>
2020-11-20 12:19:43 -0600

    subida generacion llaves

---
+++ LibreriaGenericos/Llaves.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.IO.Compression;
+
+namespace LibreriaGenericos
+{
+    class Llaves
+    {
+        public void GeneracionLlaves(int p, int q)
+        {
+            //encontrar n
+            int n = p * q;
+
+            //encontrar fi
+            int fi = (p - 1) * (q - 1);
+
+            //encontrar e
+            int e = 0, Contador = 1;
+            bool Estado = true;
+            while (Estado && Contador < fi)
+            {
+                ++Contador;
+                if (EncontrarPrimo(Contador) && fi % Contador != 0)
+                {
+                    e = Contador;
+                    Estado = false;
+                }
+            }
+
+            //encontrar d
+            int[] Arreglo1 = { fi, e };
+            int[] Arreglo2 = { fi, 1 };
+            while (Arreglo1[1] != 1)
+            {
+                int Division1 = Arreglo1[0] / Arreglo1[1];
+                int Aux1 = Arreglo1[0] - (Division1 * Arreglo1[1]);
+                int Aux2 = Arreglo2[0] - (Division1 * Arreglo2[1]);
+                if (Aux2 < 0)
+                {
+                    Aux2 = (Aux2 % fi + fi) % fi;
+                }
+                Arreglo1[0] = Arreglo1[1];
+                Arreglo1[1] = Aux1;
+                Arreglo2[0] = Arreglo2[1];
+                Arreglo2[1] = Aux2;
+            }
+            int d = Arreglo2[1];
+
+            //impresion en text files
+            string RutaPublico = "";
+            string RutaPrivada = "";
+            string LlavePublica = Convert.ToString(e) + "," + Convert.ToString(n);
+            string LlavePrivada = Convert.ToString(d) + "," + Convert.ToString(n);
+
+            File.WriteAllText(RutaPublico, LlavePublica);
+            File.WriteAllText(RutaPrivada, LlavePrivada);
+
+            //subir a zip las llaves
+            string RutaZip = "";
+
+            using (var archive = ZipFile.Open(RutaZip, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(RutaPublico, Path.GetFileName(RutaPublico));
+                archive.CreateEntryFromFile(RutaPrivada, Path.GetFileName(RutaPrivada));
+            }
+        }
+
+        public bool EncontrarPrimo(int n)
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- PruebaConsola/Program.cs
+++ PruebaConsola/Program.cs
@@ -6,6 +6,12 @@ namespace PruebaConsola
     {
         static void Main(string[] args)
         {
+            //Llaves llave = new Llaves();
+            //llave.GeneracionLlaves(307, 311);
+
+            //RSA rsa = new RSA();
+            //rsa.Cifrado("C:\\Users\\josed\\Desktop\\biblia.txt", "C:\\Users\\josed\\Desktop\\RSA\\cifrados", "C:\\Users\\josed\\Desktop\\RSA\\keys\\public.key");
+            //rsa.Descifrado("C:\\Users\\josed\\Desktop\\RSA\\cifrados\\Cifrado.txt", "C:\\Users\\josed\\Desktop\\RSA\\descifrados", "C:\\Users\\josed\\Desktop\\RSA\\keys\\private.key");
             Console.WriteLine("Hello World!");
         }
     }
